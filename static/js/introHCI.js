a054bf683b275d0b64808b61554a298ff6f0745f
Tilak Patel <tpatel@ucsd.edu>
2021-01-21 21:15:53 -0800

    Finished Lab Modifications

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
+		
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$('.jumbotron h1').addClass("active");
 	});
-
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) {
+    // prevent the page from reloading      
+    e.preventDefault();    	  
+	console.log("Project clicked"); 
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+	   $(description).fadeOut();
+		//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+}
+
+
